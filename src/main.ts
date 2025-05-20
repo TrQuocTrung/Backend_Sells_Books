7cac948c1f6e4c3f9f72d3f8b6c9acf58fcd033a
TrQuocTrung <trungtran147722@gmail.com>
2025-05-20 12:18:52 +0700

    Connect MongoDb

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'src/view'));
+  const configService = app.get(ConfigService);
+  //Config Get Port .env
+  const port = configService.get<number>('PORT');
+  MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      uri: configService.get<string>('MONGODB_URI'),
+      // connectionFactory: (connection) => {
+      //   connection.plugin(softDeletePlugin)
+      // }
+    }),
+    inject: [ConfigService],
+
+  })
+  await app.listen(configService.get<number>('PORT')!, () => {
+    console.log(`App is running at the : http://localhost:${port}`);
+  });
+
 }
 bootstrap();
